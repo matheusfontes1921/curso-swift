97f0c83978db0665969cb05ea750a5cb6ba4d684
Matheus Fontes <91166507+matheusfontes1921@users.noreply.github.com>
2023-04-12 16:45:07 -0300

    starting to create a music app model

---
+++ Matheus Fontes/Aula05Matheus/Aula05Matheus/ViewMusic.swift
@@ -0,0 +1,58 @@
+//
+//  ViewMusic.swift
+//  Aula05Matheus
+//
+//  Created by Student18 on 12/04/23.
+//
+
+import SwiftUI
+
+struct ViewMusic: View {
+    var body: some View {
+        Text("Hello, World!")
+        //        ZStack{
+        //            LinearGradient(colors: [.blue,.black], startPoint: .top, endPoint: .bottom)
+        //                .ignoresSafeArea()
+        //
+        //                Spacer()
+        //            VStack{
+        //
+        //                ForEach(pMusic){
+        //                    musicas in
+        //
+        //                Image("hacka")
+        //                Spacer()
+        //                HStack{
+        //                    Image(systemName: "shuffle")
+        //                        .foregroundColor(.white)
+        //                        .font(Font.system(.largeTitle))
+        //                        .padding(.horizontal,10.0)
+        //                    Image(systemName: "arrowshape.turn.up.backward.fill")
+        //                        .foregroundColor(.white)
+        //                        .font(Font.system(.largeTitle))
+        //                        .padding(.horizontal,10.0)
+        //                    Image(systemName: "play.fill")
+        //                        .foregroundColor(.white)
+        //                        .font(Font.system(.largeTitle))
+        //                        .padding(.horizontal,10.0)
+        //                    Image(systemName: "arrowshape.turn.up.forward.fill")
+        //                        .foregroundColor(.white)
+        //                        .font(Font.system(.largeTitle))
+        //                    .padding(.horizontal,10.0)
+        //                    Image(systemName: "repeat")
+        //                        .foregroundColor(.white)
+        //                        .font(Font.system(.largeTitle))
+        //                    .padding(.horizontal,10.0)
+        //                }
+        //                Spacer()
+        //            }
+        //        }
+        //    }
+    }
+    
+    struct ViewMusic_Previews: PreviewProvider {
+        static var previews: some View {
+            ViewMusic()
+        }
+    }
+}
